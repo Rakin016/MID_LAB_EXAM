a774c81cc25410442dc25226b692dfc11c5a254e
rakin016 <rakin016@gmail.com>
2020-08-15 23:08:39 +0600

    T3M2F4

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var userModel = require.main.require('../models/user');
+var employeeModel = require.main.require('../models/employee');
+
 var router = express.Router();
 
 router.get('*', function(req, res, next){
@@ -14,6 +16,45 @@ router.get('/', function(req, res){
 	res.render('admin/index', {uname: req.session.username});
 });
 
+
+router.get('/allEmployeeList',function (req,res){
+    if(req.session.username!=null){
+        employeeModel.getAll(function (results){
+            var data ={results:results}
+            console.log(data);
+            res.render('admin/allEmp',data);
+        });
+
+    }
+    else {
+
+    }
+});
+
+router.get('/update/:id',function (req,res) {
+    if(req.session.username!=null){
+        employeeModel.get(req.params.id,function (result) {
+            console.log(result);
+            res.render('admin/update',result);
+        });
+    }else {
+        res.redirect('/login');
+    }
+});
+
+router.post('/update/:id',function (req,res) {
+    console.log(req.body);
+    var emp=req.body;
+    emp.id=req.params.id;
+    employeeModel.update(emp,function(status){
+        if(status){
+            res.redirect('/admin/allEmployeeList');
+        }
+        else{
+            res.send("All fields required");
+        }
+    });
+});
 
 
 module.exports = router;
